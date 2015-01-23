a0f69c2794e453bc252cd393fa8007a0ed3ac88c
David Charnuska <charnushka@Davids-MacBook-Pro.local>
2015-01-23 00:23:02 -0500

    add update and remove functionality

--- simple-todos.js
+++ simple-todos.js
@@ -4,12 +4,13 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1 }});
     }
   });
 
   Template.body.events({
     "submit .new-task": function (event) {
+      console.log(event);
       var text = event.target.text.value;
 
       Tasks.insert({
@@ -17,11 +18,21 @@ if (Meteor.isClient) {
         createdAt: new Date()
       });
 
+      console.log(Tasks.last());
       // Clear form
       event.target.text.value = "";
 
       // Prevent default form submit
       return false;
     }
-  })
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
