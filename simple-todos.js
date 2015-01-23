56e5ee3ed59ddca3e3b211002b28f2fcf4071b64
David Charnuska <charnushka@Davids-MacBook-Pro.local>
2015-01-22 23:27:28 -0500

    initial commit

---
+++ simple-todos.js
@@ -0,0 +1,27 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  })
+}
