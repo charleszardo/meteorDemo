b0c45431413edb3472783bdfa57911557e4ed8c8
David Charnuska <charnushka@Davids-MacBook-Pro.local>
2015-01-23 00:29:14 -0500

    fix update/remove functionality

--- simple-todos.js
+++ simple-todos.js
@@ -31,6 +31,7 @@ if (Meteor.isClient) {
     "click .toggle-checked": function () {
       Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
+    
     "click .delete": function () {
       Tasks.remove(this._id);
     }
